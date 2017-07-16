b0ca707b61bf8e35cb7e118ce16631e9eec3808e
MoHuiTou <715782505@qq.com>
2017-07-17 00:36:39 +0800

    node >=4

--- test/test.js
+++ test/test.js
@@ -1,11 +1,21 @@
 import test from 'ava';
-
-test('foo', t => {
-	t.pass();
-});
-
-test('bar', async t => {
-	const bar = Promise.resolve('bar');
-
-	t.is(await bar, 'bar');
+import webpack from 'webpack';
+// 0. Import the config that uses my plugin
+import options from '../example/webpack.config.js';
+test.cb('Compiles routes nested at one level', t => {
+  // 1. Run webpack
+  webpack(options, function(err, stats) {
+    // 2. Fail test if there are errors
+    if (err) {
+      return t.end(err);
+    } else if (stats.hasErrors()) {
+      return t.end(stats.toString());
+    }
+    // 3. Map asset objects to output filenames
+    const files = stats.toJson().assets.map(x => x.name);
+    // 4. Run assertions. Make sure that the three expected
+    //    HTML files were generated
+    t.true(files.indexOf('bundle.js') !== -1);
+    t.end();
+  });
 });
